5c156b6917466ccf3c51d8f3fc392c1bec51afd0
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-06-26 22:07:36 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 1500 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,74 +1,70 @@
 #include <stdio.h>
 
-int heap[300001]; // root = 1 
-int size;
+int Heap[100001], size;
 
-void push(int item){
-	heap[++size] = item; // heap의 마지막에 아이템 추가 
-	// 부모 자식 관계 부모는 자식의 index / 2
-	// 자식은 부모의 index *2(왼쪽 자식) 또는 index * 2 + 1(오른쪽 자식)
-	int now = size ;
-	while(now>1 && heap[now] > heap[now/2]){
-		int tmp = heap[now]; // swap 코드 
-		heap[now] = heap[now/2];
-		heap[now/2] = tmp;
-		now/=2;
-	}	
-}
-int pop(){
-    if(size==0)
-        return 0;
-    int returnValue = heap[1];
-    heap[1] = heap[size];
-    size--;
-    int now = 1;
-    while(now<=size){
-        if(now*2>size)return returnValue;
-        else if(now*2+1>size){
-            if(heap[now] < heap[now*2]){
-                int tmp = heap[now];
-                heap[now] = heap[now*2];
-                heap[now*2]=tmp;
-                now*=2;
-            }
-            else return returnValue;
-        }
-        else{
-            if(heap[now*2] < heap[now*2+1]){
-                if(heap[now*2+1] > heap[now]){
-                    int tmp = heap[now];
-                    heap[now] = heap[now*2+1];
-                    heap[now*2+1] = tmp;
-                    now = now*2+1;
-                }
-                else return returnValue;
-            }
-            else{
-                if(heap[now*2] > heap[now]){
-                    int tmp = heap[now];
-                    heap[now] = heap[now*2];
-                    heap[now*2] = tmp;
-                    now = now*2;
-                }
-                else return returnValue;
-            }
-        }
-    }
-	return returnValue;
+void Push(int item) {
+   Heap[++size] = item;
+   int now = size;
+   while (now > 1 && Heap[now] > Heap[now/2]) {
+      int tmp = Heap[now];
+      Heap[now] = Heap[now / 2];
+      Heap[now / 2] = tmp;
+      now /= 2;
+   }
 }
 
-int main() {
-	int query,inst;
-	scanf("%d",&query);
-	while(query--){
-		scanf("%d",&inst);
-		if(inst){
-			push(inst);
-		}
-		else{
-			printf("%d\n",pop());
-		}
-	}
+int Pop() {
+   if (size == 0)
+      return 0;
+   int rtnVal = Heap[1];
+   Heap[1] = Heap[size];
+   size--;
+   int now = 1;
+   while (now <= size) {
+      if (now * 2 > size)
+         return rtnVal;
+      else if (now * 2 + 1 > size) {
+         if (Heap[now] < Heap[now * 2]) {
+            int tmp = Heap[now];
+            Heap[now] = Heap[now * 2];
+            Heap[now * 2] = tmp;
+            now *= 2;
+         }
+         else
+            return rtnVal;
+      }
+      else {
+         if (Heap[now * 2] < Heap[now * 2 + 1]){
+            if (Heap[now * 2 + 1] > Heap[now]) {
+               int tmp = Heap[now];
+               Heap[now] = Heap[now * 2 + 1];
+               Heap[now * 2 + 1] = tmp;
+               now = now * 2 + 1;
+            }
+			else return rtnVal;
+         }
+         else{
+            if (Heap[now * 2] > Heap[now]) {
+               int tmp = Heap[now];
+               Heap[now] = Heap[now * 2];
+               Heap[now * 2] = tmp;
+               now *= 2;
+            }
+			else return rtnVal;
+		 }
+      }
+   }
+   return rtnVal;
+}
 
-    return 0;
+int main(void) {
+   int ask, inst;
+   scanf("%d", &ask);
+   while (ask--) {
+      scanf("%d", &inst);
+      if (inst)
+         Push(inst);
+      else
+         printf("%d\n", Pop());
+   }
 }
