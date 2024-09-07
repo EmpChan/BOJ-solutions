bdf8dacdae1c9d91f5f22cfb2f6900dd93456244
JaeChanHwang <57826563+EmpChan@users.noreply.github.com>
2024-09-07 10:51:33 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,10 +1,8 @@
-s = list(input())
-a = list(input())
-while(len(s) < len(a)):
-    if a[-1] == 'A':
-        a.pop()
-    else:
-        a.pop()
-        a = list(reversed(a))
-
+I=input
+L=list
+s=L(I())
+a=L(I())
+while len(s)<len(a):
+ if a[-1]=='A':a.pop()
+ else:a.pop();a=L(reversed(a))
 print(int(a==s))
